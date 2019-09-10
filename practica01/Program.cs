f22dfbb8c279a4f76f366a7cdde250ad6d209e67
@bidkar <bidkar@gmail.com>
2019-09-10 15:18:49 -0600

    Proyecto inicializado para MySQL

---
+++ practica01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practica01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
